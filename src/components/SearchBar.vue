8e4651caa2c39c25400e78340fd686e52465adbb
Ahmadreza Ahmadi <ahmadrezaahmadi.dev@gmail.com>
2023-05-27 20:33:31 +0330

    Solving the problem of not adding files in commit baf1e75

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps(['modelValue']);
+const emit = defineEmits(['update:modelValue']);
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+  },
+});
+</script>
+
+<template>
+  <div>
+    <input
+      v-model="value"
+      type="text"
+      placeholder="Search for a country"
+      class="form-control px-3 py-2"
+    />
+  </div>
+</template>
